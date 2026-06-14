c13c1ff909cad58aa7d888fd648c11536f16bf76
Leif Johansson <leifj@mnt.se>
2026-06-14 17:02:07 +0200

    Add coverage tracking and Dokka documentation (#11)
    - Add Codecov upload to CI (Kover XML reports)
    - Add codecov badge to README
    - Add Dokka plugin for KDoc documentation generation
    - Add docs CI job running dokkaGenerate
    - Add Documentation section to README
    
    Co-authored-by: Leif Johansson <leifj@sunet.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kover)
+    alias(libs.plugins.dokka)
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlinx.kover")
+    apply(plugin = "org.jetbrains.dokka")
 }
