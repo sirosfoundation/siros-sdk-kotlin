eda86026893603986c4345e465a8092b07d8b270
Leif Johansson <leifj@mnt.se>
2026-07-21 23:44:47 +0200

    chore(sample-app): bump versionCode to 4 for Play Store re-upload

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "org.siros.sdk.sample"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
