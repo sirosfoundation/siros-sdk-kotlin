4cd2a87c430068a865edc0a96e6a183abc40a893
Leif Johansson <leifj@mnt.se>
2026-08-31 12:25:52 +0200

    chore(release): bump to v0.10.0 and roll CHANGELOG forward (#146)

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.siros.sdk.sample"
         minSdk = 28
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "0.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
